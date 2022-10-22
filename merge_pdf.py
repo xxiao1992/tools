f7c79ffb63fe801091f8f470a19611f4241b67ca
xxiao1992 <xx2226@columbia.edu>
2022-10-22 16:04:35 -0400

    initial commit

---
+++ merge_pdf.py
@@ -0,0 +1,22 @@
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+import os
+
+
+filenames = [x for x in os.listdir() if 'pdf' in x.lower() and 'To_print' not in x]
+
+files = []
+outPdf = PdfFileWriter()
+for filename in filenames:
+    f = open(filename, 'rb')
+    files.append(f)
+    file = PdfFileReader(f, strict = False)
+    outPdf.appendPagesFromReader(file)
+    n_page = file.numPages
+    if n_page % 2 == 1:
+        outPdf.addBlankPage()
+
+with open('To_print.pdf','wb') as outStream:
+    outPdf.write(outStream)
+
+for x in files:
+    x.close()
