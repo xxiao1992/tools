0f21d6c7174702500846eae67891e73be60fb32b
Xuyan <xx2226@columbia.edu>
2024-03-10 22:46:07 -0400

    compatibility with PyPDF2 3.0

--- merge.py
+++ merge.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfReader, PdfWriter
 import os
 
 
@@ -6,15 +6,15 @@ filenames = [x for x in os.listdir() if 'pdf' in x.lower() and 'To_print' not in
 filenames = list(sorted(filenames))
 
 files = []
-outPdf = PdfFileWriter()
+outPdf = PdfWriter()
 for filename in filenames:
     f = open(filename, 'rb')
     files.append(f)
-    file = PdfFileReader(f, strict=False)
-    outPdf.appendPagesFromReader(file)
-    n_page = file.numPages
+    file = PdfReader(f, strict=False)
+    outPdf.append_pages_from_reader(file)
+    n_page = len(file.pages)
     if n_page % 2 == 1:
-        outPdf.addBlankPage()
+        outPdf.add_blank_page()
 
 with open('To_print.pdf', 'wb') as outStream:
     outPdf.write(outStream)
