031828047c4b1988d6390002a0f929083d4c907e
Xuyan <xx2226@columbia.edu>
2024-05-12 23:20:53 -0400

    use pypdf

--- merge.py
+++ merge.py
@@ -1,14 +1,14 @@
-from PyPDF2 import PdfReader, PdfWriter
 import os
 
+from pypdf import PdfReader, PdfWriter
 
-filenames = [x for x in os.listdir() if 'pdf' in x.lower() and 'To_print' not in x]
+filenames = [x for x in os.listdir() if "pdf" in x.lower() and "To_print" not in x]
 filenames = list(sorted(filenames))
 
 files = []
 outPdf = PdfWriter()
 for filename in filenames:
-    f = open(filename, 'rb')
+    f = open(filename, "rb")
     files.append(f)
     file = PdfReader(f, strict=False)
     outPdf.append_pages_from_reader(file)
@@ -16,7 +16,7 @@ for filename in filenames:
     if n_page % 2 == 1:
         outPdf.add_blank_page()
 
-with open('To_print.pdf', 'wb') as outStream:
+with open("To_print.pdf", "wb") as outStream:
     outPdf.write(outStream)
 
 for x in files:
